2f9269f7c610e87d7c3fdc534a86b686114e679f
Timur Moiseenko <angrav6@gmail.com>
2025-01-14 08:50:47 +0500

    add assets at Cropper.php

--- src/Fields/Cropper.php
+++ src/Fields/Cropper.php
@@ -8,4 +8,11 @@ class Cropper extends Image
 {
     protected string $view = 'moonshine-cropper::fields.cropper';
     protected string $accept = 'image/*';
+
+    protected array $assets = [
+        'vendor/moonshine-cropper/css/cropper.min.css',
+        'vendor/moonshine-cropper/css/moonshine-cropper.css',
+        'vendor/moonshine-cropper/js/cropper.min.js',
+        'vendor/moonshine-cropper/js/cropper-init.js',
+    ];
 }
